7a27bfc358188a1db6ae1c2980f3e6e0248f9416
ColonelBlimp <ColonelBlimp@users.noreply.github.com>
2021-05-16 18:57:02 +0200

    Latest

---
+++ system/core/classes/i18n/i18n.php
@@ -0,0 +1,24 @@
+<?php declare(strict_types=1);
+namespace html_go\i18n;
+
+final class i18n
+{
+    /**
+     * @var array<string, string>
+     */
+    private array $bundle = [];
+
+    function __construct(string $bundlePath) {
+        if (!\file_exists($bundlePath)) {
+            throw new \InvalidArgumentException("i18n bundle file not found [$bundlePath]");
+        }
+        $this->bundle = include $bundlePath;
+    }
+
+    function getText(string $key): string {
+        if (\array_key_exists($key, $this->bundle)) {
+            return $this->bundle[$key];
+        }
+        return '!'.$key.'!';
+    }
+}
