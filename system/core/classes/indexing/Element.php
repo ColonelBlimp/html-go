bb96b4894456d6e19ce779bb2ea52a0ff47c1043
ColonelBlimp <ColonelBlimp@users.noreply.github.com>
2021-05-20 14:49:53 +0200

    Latest

--- system/core/classes/indexing/Element.php
+++ system/core/classes/indexing/Element.php
@@ -23,27 +23,67 @@ final class Element
         private string $date,
         private array $tagList) {
     }
+
+    /**
+     * The unique name for this content.
+     * @return string
+     */
     function getKey(): string {
         return $this->key;
     }
+
+    /**
+     * The fullpath for the content file.
+     * @return string
+     */
     function getPath(): string {
         return $this->path;
     }
+
+    /**
+     * The section for this content. This will be either 'pages', 'posts' or 'categories'
+     * @return string
+     */
     function getSection(): string {
         return $this->section;
     }
+
+    /**
+     * This relates to 'posts' only and indicates the category for post.
+     * @return string
+     */
     function getCategory(): string {
         return $this->category;
     }
+
+    /**
+     * This relates to 'posts' only and indicates the type of post (regular, image, etc).
+     * @return string
+     */
     function getType(): string {
         return $this->type;
     }
+
+    /**
+     * The system username under which this content is located. This relates to 'posts' only.
+     * @return string
+     */
     function getUsername(): string {
         return $this->username;
     }
+
+    /**
+     * The date string from the filename. This relates to 'posts' only.
+     * @return string
+     */
     function getDate(): string {
         return $this->date;
     }
+
+    /**
+     * Returns an array of tag slugs. This relates to 'posts' only.
+     * @return array<int, string>
+     */
     function getTags(): array {
         return $this->tagList;
     }
