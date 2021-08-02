0aa2fba0462d8caf71c3566473f62f97c1926371
ColonelBlimp <ColonelBlimp@users.noreply.github.com>
2021-08-02 13:19:00 +0200

    Latest

--- index.php
+++ index.php
@@ -3,6 +3,9 @@
 use html_go\model\Config;
 
 include __DIR__.DIRECTORY_SEPARATOR.'system'.DIRECTORY_SEPARATOR.'vendor'.DIRECTORY_SEPARATOR.'autoload.php';
+
+\set_exception_handler('exception_handler');
+
 if (getenv('CLI_ENV', true)) {
 //    echo dispatch(HOME_INDEX_KEY.FWD_SLASH.get_config()->getString(Config::KEY_ADMIN_CONTEXT));
 //    echo dispatch('admin/category/edit?id=category/uncategorized');
